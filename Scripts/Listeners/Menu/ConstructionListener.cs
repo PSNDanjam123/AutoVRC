769ff3856937b8e784ae055f4f1e5404fce135af
Jamie Bailey <psndanjam123@gmail.com>
2023-03-12 16:27:03 +0000

    added shop refresh button

---
+++ Scripts/Listeners/Menu/ConstructionListener.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+using AutoVRC.Framework;
+
+namespace AutoVRC.Listeners.Menu
+{
+    public class ConstructionListener : Listener
+    {
+        [Header("Models")]
+        public Models.GameMaster GameMaster;
+
+        public override void OnBootstrap()
+        {
+            Subscribe(GameMaster);
+        }
+
+        public override void OnModelSync()
+        {
+            gameObject.SetActive(GameMaster.GameInProgress);
+        }
+
+    }
+
+}
